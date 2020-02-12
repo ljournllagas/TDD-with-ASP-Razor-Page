5d2a0fe2fd81f445708a36f8c96fb6aa8864d22a
Ljourn Llagas <40813696+ljournllagas@users.noreply.github.com>
2020-02-12 15:01:44 +0800

    Completed the test cases for BookDesk business logic

--- DeskBooker.Web/Pages/BookDesk.cshtml.cs
+++ DeskBooker.Web/Pages/BookDesk.cshtml.cs
@@ -2,6 +2,7 @@
 using DeskBooker.Core.Processor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace DeskBooker.Web.Pages
 {
@@ -17,13 +18,29 @@ namespace DeskBooker.Web.Pages
         [BindProperty]
         public DeskBookingRequest DeskBookingRequest { get; set; }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
+            IActionResult actionResult = Page();
+
             if (ModelState.IsValid)
             {
-                _deskBookingRequestProcessor.BookDesk(DeskBookingRequest);
+                var result = _deskBookingRequestProcessor.BookDesk(DeskBookingRequest);
+
+                if (result.Code == DeskBookingResultCode.Success)
+                {
+                    actionResult = RedirectToPage("BookDeskConfirmation", new {
+                        result.DeskBookingId,
+                        result.FirstName,
+                        result.Date
+                    });
+                }
+                else if (result.Code == DeskBookingResultCode.NoDeskAvailable)
+                {
+                    ModelState.AddModelError("DeskBookingRequest.Date", "No desk available for selected date");
+                }
             }
 
+            return actionResult;
         }
   }
 }
