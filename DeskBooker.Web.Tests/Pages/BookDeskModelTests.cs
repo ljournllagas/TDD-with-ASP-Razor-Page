d82c64319ee475aa17fa32ff491eed164c4be46f
Ljourn Llagas <40813696+ljournllagas@users.noreply.github.com>
2020-02-12 12:02:44 +0800

    Add project files.

---
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -0,0 +1,61 @@
+﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using Moq;
+using Xunit;
+
+namespace DeskBooker.Web.Pages
+{
+  public class BookDeskModelTests
+  {
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(0, false)]
+        public void ShouldCallBookDeskMethodOfProcessorIfModelIsValid(int expectedBookDeskCalls, bool isModelValid)
+        {
+            // Arrange
+            var processorMock = new Mock<IDeskBookingRequestProcessor>();
+
+            var bookDeskModel = new BookDeskModel(processorMock.Object)
+            {
+                DeskBookingRequest = new DeskBookingRequest()
+            };
+
+            if (!isModelValid)
+            {
+                bookDeskModel.ModelState.AddModelError("Key", "An Error Message");
+            }
+
+            // Act
+            bookDeskModel.OnPost();
+
+            // Assert
+            processorMock.Verify(x => x.BookDesk(bookDeskModel.DeskBookingRequest), Times.Exactly(expectedBookDeskCalls));
+        }
+
+        [Fact]
+        public void ShouldAddModelErrorIfNoDeskIsAvailable()
+        {
+            var processorMock = new Mock<IDeskBookingRequestProcessor>();
+
+            var bookDeskModel = new BookDeskModel(processorMock.Object)
+            {
+                DeskBookingRequest = new DeskBookingRequest()
+            };
+
+            processorMock.Setup(x => x.BookDesk(bookDeskModel.DeskBookingRequest))
+                .Returns(new DeskBookingResult
+                {
+                    Code = DeskBookingResultCode.NoDeskAvailable
+                });
+
+            // Act
+            bookDeskModel.OnPost();
+
+            // Assert
+            var modelStateEntry = Assert.Contains("DeskBookingRequest.Data", bookDeskModel.ModelState);
+            var modelError = Assert.Single(modelStateEntry.Errors);
+            Assert.Equal("No desk available for selected date", modelError.ErrorMessage);
+
+        }
+  }
+}
---
+++ DeskBooker.Web/Pages/BookDesk.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DeskBooker.Web.Pages
+{
+  public class BookDeskModel : PageModel
+  {
+        private IDeskBookingRequestProcessor _deskBookingRequestProcessor;
+
+        public BookDeskModel(IDeskBookingRequestProcessor deskBookingRequestProcessor)
+        {
+            _deskBookingRequestProcessor = deskBookingRequestProcessor;
+        }
+
+        [BindProperty]
+        public DeskBookingRequest DeskBookingRequest { get; set; }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _deskBookingRequestProcessor.BookDesk(DeskBookingRequest);
+            }
+
+        }
+  }
+}
